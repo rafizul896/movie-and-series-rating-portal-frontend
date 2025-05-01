241e0f13dd58d7133b0650154ac17cf7f3f423b9
sajjad <sajjadsajjad098765@gmail.com>
2025-05-01 12:57:42 +0600

    text added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ const HomePage = () => {
     <div>
       <h1>This is Home Page</h1>
       <Button>Check</Button>
+
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam laudantium saepe neque voluptate repudiandae amet dolor est facilis dignissimos nam!</p>
     </div>
   );
 };
