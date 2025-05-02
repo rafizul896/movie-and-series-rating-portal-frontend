756834fd87a3d2512b365871397ea0bb0f327e1b
opu183059 <opuctg440@gamil.com>
2025-05-02 18:15:14 +0600

    Added New Arrived, Top rated, Editors pic in the home page

--- src/app/(withCommonLayout)/(home)/page.tsx
+++ src/app/(withCommonLayout)/(home)/page.tsx
@@ -1,9 +1,15 @@
 import Banner from "@/components/modules/home/Banner";
+import EditorsPick from "@/components/modules/home/EditorsPick";
+import NewArrived from "@/components/modules/home/NewArrived";
+import TopRated from "@/components/modules/home/TopRated";
 
 const HomePage = () => {
   return (
     <div>
       <Banner />
+      <NewArrived />
+      <TopRated />
+      <EditorsPick />
     </div>
   );
 };
